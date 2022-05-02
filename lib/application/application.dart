91c50c49fee00ed9b516a2d5ed0032038330f030
오진수 <ohjinsu@codersproduct.com>
2022-05-02 20:59:05 +0900

    Add the env

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -1,12 +1,15 @@
 import 'package:album/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
   void bootstrap() async {
+    await DotEnv().load();
+
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
@@ -14,6 +17,7 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bootstrap();
     return GetMaterialApp(
       builder: (context, child) {
         return MediaQuery(
