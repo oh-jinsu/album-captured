7f78e0e4def8d9c7bc65e94eb4babfe424469e3b
오진수 <ohjinsu@codersproduct.com>
2022-05-05 02:57:48 +0900

    Move the bootstrapper

--- lib/application/usecases/bootstrap.dart
+++ lib/application/usecases/bootstrap.dart
@@ -1,10 +1,43 @@
 import 'package:album/application/controller.dart';
 import 'package:album/core/action.dart';
+import 'package:album/core/navigator.dart';
 import 'package:album/core/usecase.dart';
+import 'package:album/firebase_options.dart';
+import 'package:album/repositories/auth.dart';
+import 'package:album/utils/fetch.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class BootstrapUseCase extends UseCase {
   @override
   void onAwake() {
-    of<App>().on<Created>((action) {});
+    of<App>().on<Created>((action) async {
+      await DotEnv().load();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+
+      final accessToken = await authRepository.findAccessToken();
+
+      if (accessToken == null) {
+        final response =
+            await get(Uri.parse("http://localhost:3000/v1/auth/guest"));
+
+        if (response is! SuccessResponse) {
+          return;
+        }
+
+        final accessToken = response.body["access_token"];
+
+        authRepository.saveAccessToken(accessToken);
+
+        await post(Uri.parse("http://localhost:3000/v1/user/guest"), headers: {
+          "Authorization": "Bearer $accessToken",
+        });
+      }
+
+      requireNavigator().pushNamed("/home");
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:album/application/controller.dart';
+import 'package:album/application/usecases/bootstrap.dart';
 import 'package:album/core/locator.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(Provider(controller: const App()));
+  runApp(
+    Provider(
+      usecases: [
+        BootstrapUseCase(),
+      ],
+      controller: const App(),
+    ),
+  );
 }
