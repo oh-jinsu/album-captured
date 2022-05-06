9c607458396614f6c4159d7c390eb3e5bf0ffa3d
오진수 <ohjinsu@codersproduct.com>
2022-05-06 12:16:02 +0900

    Sign in automatically

--- lib/application/usecases/bootstrap.dart
+++ lib/application/usecases/bootstrap.dart
@@ -10,7 +10,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class BootstrapUseCase extends UseCase {
-  late final authRepository = use<AuthRepository>();
   @override
   void onAwaken() {
     of<App>().on<Created>((event) async {
@@ -20,9 +19,9 @@ class BootstrapUseCase extends UseCase {
         options: DefaultFirebaseOptions.currentPlatform,
       );
 
-      final accessToken = await use<AuthRepository>().findAccessToken();
+      final refreshToken = await use<AuthRepository>().findRefreshToken();
 
-      if (accessToken == null) {
+      if (refreshToken == null) {
         final response = await use<Client>()
             .get(Uri.parse("http://localhost:3000/v1/auth/guest"));
 
@@ -32,7 +31,11 @@ class BootstrapUseCase extends UseCase {
 
         final accessToken = response.body["access_token"];
 
+        final refreshToken = response.body["refresh_token"];
+
         use<AuthRepository>().saveAccessToken(accessToken);
+
+        use<AuthRepository>().saveRefreshToken(refreshToken);
 
         await use<Client>().post(
           Uri.parse("http://localhost:3000/v1/user/guest"),
@@ -40,6 +43,21 @@ class BootstrapUseCase extends UseCase {
             "Authorization": "Bearer $accessToken",
           },
         );
+      } else {
+        final response = await use<Client>().post(
+          Uri.parse("http://localhost:3000/v1/auth/refresh"),
+          body: {
+            "refresh_token": refreshToken,
+          },
+        );
+
+        if (response is! SuccessResponse) {
+          return;
+        }
+
+        final accessToken = response.body["access_token"];
+
+        use<AuthRepository>().saveAccessToken(accessToken);
       }
 
       of<Splash>().dispatch(const Pushed("/home"));
